1c70919d61d1bff911595ec469b810b24f75b42c
Howard Lichtman <Lichtmanh@hartwick.edu>
2019-12-06 23:20:20 -0500

    Update

--- Person.py
+++ Person.py
@@ -3,7 +3,6 @@ class Person:
     def __init__(self, first, last):
         self.first_name = first
         self.last_name = last
-        self.age = age
 
     def description(self):
         return self.first_name + " " + self.last_name
